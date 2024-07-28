9133d57619dcf02a3ae2254b170929891deff90c
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-07-27 22:20:11 -0600

    Add basic migration and model for Applicants

---
+++ app/Models/Applicant.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\Permission\Traits\HasRoles;
+
+class Applicant extends Authenticatable implements HasMedia
+{
+    use HasFactory, HasRoles, InteractsWithMedia, Notifiable;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'country',
+        'status',
+        ];
+}
