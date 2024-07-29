44874c1f8e64a4ce4661297885fd83f7615b4372
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-07-28 21:17:29 -0600

    Add ApplicantSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-        ]);
+        if(config('app.env') === 'local' || config('app.env') === 'staging') {
+            $this->call([
+                UserSeeder::class,
+                ApplicantSeeder::class,
+            ]);
+        }
     }
 }
