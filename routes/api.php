23901085843b61d268c4597d71bc114bbad3b61f
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-07-29 17:10:25 -0600

    Apply Sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
+use App\Http\Controllers\api\v1\ApplicantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\api\v1\ApplicantController;
-
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => '/v1', 'as' => 'api.'], function () {
+Route::middleware(['auth:sanctum'])->prefix('/v1')->as('api.')->group(function () {
     route::apiResource('/applicants', ApplicantController::class);
 });
