97e6c909848a39c29a8e90e46b2b36acffe16e71
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-07-29 12:57:15 -0600

    Add Enum for applicants Status Update migration for applicants

--- database/migrations/2024_07_28_041100_create_applicants_table.php
+++ database/migrations/2024_07_28_041100_create_applicants_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enum\ApplicantStateEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,12 +17,13 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
-            $table->string('phone');
-            $table->string('address');
-            $table->string('city');
-            $table->string('state');
-            $table->string('country');
-            $table->string('status');
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('status')->default(ApplicantStateEnum::ACTIVE->value);
             $table->timestamps();
             $table->softDeletes();
         });
