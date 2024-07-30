722675297bd63fed2340a3009f35e4ee0db80370
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-07-30 13:38:02 -0600

    Add routes and controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\v1\ApplicantController;
+use App\Http\Controllers\api\v1\SkillController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->prefix('/v1')->as('api.')->group(function () {
     route::apiResource('/applicants', ApplicantController::class);
+    route::apiResource('/skills', SkillController::class);
 });
