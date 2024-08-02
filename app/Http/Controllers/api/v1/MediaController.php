06c66e98796b0ba1a18257c944ad090331a1af6c
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-08-02 00:19:20 -0600

    Add contract for MediaService and implement logics

---
+++ app/Http/Controllers/api/v1/MediaController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Contracts\MediaServiceInterface;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\MediaRequest;
+use App\Http\Resources\MediaResource;
+use App\Models\Media;
+use App\Services\MediaService;
+use Illuminate\Http\Request;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    private MediaServiceInterface $service;
+
+    public function __construct()
+    {
+        $this->service = new MediaService();
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(MediaRequest $request)
+    {
+        $validated = $request->validated();
+        return new MediaResource($this->service->create(auth()->user(), $validated['media'], $validated['collection_name']));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Media $media)
+    {
+        $this->service->delete($media);
+
+        return response()->noContent();
+    }
+}
