7d6be0ee1ed5d6bc1b08211a4ec9176d88ad2378
Ismael Ramos Dumas <iramosdev@hotmail.com>
2024-08-02 00:10:14 -0600

    Add test for MediaService.php

---
+++ tests/Unit/Media/MediaServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Applicant;
+use App\Models\Media;
+use App\Services\MediaService;
+use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
+
+it('can create and delete media', function () {
+    $fullPath = storage_path('/app/public/testing-files/test-file.pdf');
+
+    touch($fullPath);
+
+    $response = (new MediaService())->create(
+    Applicant::factory()->create(),
+    $fullPath,
+    'collection_name'
+    );
+
+    $this->assertTrue($response instanceof BaseMedia);
+
+    $media = Media::find($response->id);
+    $this->assertTrue(
+        (new MediaService())->delete($media)
+    );
+});
